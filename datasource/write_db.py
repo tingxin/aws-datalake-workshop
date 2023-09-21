91ede2aa798c70dbf4aff1d73b1baaedb116d654
EC2 Default User <ec2-user@ip-172-31-13-201.ap-northeast-1.compute.internal>
2023-09-21 12:17:23 +0000

    update

--- datasource/write_db.py
+++ datasource/write_db.py
@@ -10,22 +10,22 @@ focus_database = setting.DB
 conn = get_conn(focus_database)
 
 order_schema = {
+    "goods_id": (DataType.INT,),
     "user_mail": (DataType.Enum, ('barry.xu@163.com', 'dandan@qq.com', 'pony@qq.com', 'focus@qq.com')),
     "status": (DataType.Enum, ('unpaid', 'paid', 'cancel', 'shipping', 'finished')),
     "good_count": (DataType.INT, (1, 10)),
-    "city":  (DataType.CITY,),
     "amount": (DataType.DOUBLE, (10, 1000)),
     "create_time": (DataType.DATETIME,),
     "update_time": (DataType.DATETIME,)
 }
 
 creator = gen(columns=order_schema,
-              interval_min=100, interval_max=1000)
+              interval_min=100, interval_max=1000, increment_id='order_id')
 
 for item in creator:
     print(item)
     try:
-        command = Insert("`{0}`".format("order"))
+        command = Insert("`{0}`".format("order_event"))
         for key in item:
             command.put(key, item[key])
 
